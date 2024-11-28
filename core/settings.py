b05d537299821848b722afa3bd949e0972e724d1
saminmahmud <samin.mahmud18@gmail.com>
2024-11-28 10:52:15 +0600

    change media file again

--- core/settings.py
+++ core/settings.py
@@ -170,7 +170,7 @@ if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 else:
-
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
